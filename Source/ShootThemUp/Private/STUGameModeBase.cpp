fb4ead92498b148f0c16f745aafd35e1dd43cbb1
s_timokhin <s_timokhin@wargaming.net>
2023-08-23 10:43:06 +0300

    game mode add configuration

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -4,10 +4,91 @@
 #include "Player/STUBaseCharacter.h"
 #include "Player/STUPlayerController.h"
 #include "UI/STUGameHUD.h"
+#include "AIController.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogSTUGameModeBase, All, All);
 
 ASTUGameModeBase::ASTUGameModeBase()
 {
     DefaultPawnClass = ASTUBaseCharacter::StaticClass();
     PlayerControllerClass = ASTUPlayerController::StaticClass();
     HUDClass = ASTUGameHUD::StaticClass();
+}
+
+void ASTUGameModeBase::StartPlay()
+{
+    Super::StartPlay();
+
+    SpawnBots();
+    CurrentRound = 1;
+    StartRound();
+}
+
+void ASTUGameModeBase::SpawnBots()
+{
+    if (!GetWorld()) return;
+
+    for (int32 i = 0; i < GameData.PlayersNum - 1; ++i)
+    {
+        FActorSpawnParameters SpawnInfo;
+        SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+        const auto STUAIController = GetWorld()->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+        RestartPlayer(STUAIController);
+    }
+}
+
+UClass* ASTUGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+    if (InController && InController->IsA<AAIController>())
+    {
+        return AIPawnClass;
+    }
+    return Super::GetDefaultPawnClassForController_Implementation(InController);
+}
+
+void ASTUGameModeBase::StartRound()
+{
+    RoundCountDown = GameData.RoundTime;
+    GetWorldTimerManager().SetTimer(GameRoundTimerHandle, this, &ASTUGameModeBase::GameTimerUpdate, 1.0f, true);
+}
+void ASTUGameModeBase::GameTimerUpdate()
+{
+    UE_LOG(LogSTUGameModeBase, Display, TEXT("Time: %i / Round: %i/%i"), RoundCountDown, CurrentRound, GameData.RoundsNum);
+
+    // const auto TimerRate = GetWorldTimerManager().GetTimerRate(GameRoundTimerHandle); we can use this approach
+    // RoundCountDown -= TimerRate;
+
+    if (--RoundCountDown == 0)
+    {
+        GetWorldTimerManager().ClearTimer(GameRoundTimerHandle);
+        if (CurrentRound + 1 <= GameData.RoundsNum)
+        {
+            ++CurrentRound;
+            ResetPlayers();
+            StartRound();
+        }
+        else
+        {
+            UE_LOG(LogSTUGameModeBase, Display, TEXT("=============== GAME OVER ============="));
+        }
+    }
+}
+
+void ASTUGameModeBase::ResetPlayers()
+{
+    if (!GetWorld()) return;
+
+    for (auto It = GetWorld()->GetControllerIterator(); It; ++It)
+    {
+        ResetOnePlayer(It->Get());
+    }
+}
+void ASTUGameModeBase::ResetOnePlayer(AController* Controller)
+{
+    if (Controller && Controller->GetPawn())
+    {
+        Controller->GetPawn()->Reset();
+    }
+    RestartPlayer(Controller);
 }
